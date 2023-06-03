9ac95128d35dd1861bff1b8ec52da1190dd96a39
frankerF <frankergamer@outlook.com>
2023-06-03 19:01:51 +0000

    agregada funcion translate.py

---
+++ src/translate.py
@@ -0,0 +1,24 @@
+import json
+import decimalencoder
+import todoList
+
+
+def translate(event, context):
+    
+    item = todoList.get_item(event['pathParameters']['id'])
+    if item:
+        language = event['pathParameters']['language']
+        itemTranslated = todoList.get_translate_item(item['id'],language)
+        print ("\nSalida de itemTranslated: "+ str(itemTranslated)+ "\n")
+        response = {
+            "statusCode": 200,
+            #"body": str(itemTranslated['TranslatedText'])
+             "body": json.dumps(itemTranslated,
+                               cls=decimalencoder.DecimalEncoder)
+        }
+    else:
+        response = {
+            "statusCode": 404,
+            "body": ""
+        }
+    return response
