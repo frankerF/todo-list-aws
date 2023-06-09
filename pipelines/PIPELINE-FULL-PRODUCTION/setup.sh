92068d6985273b042fa47ea064fa1176b3cdbaef
frankerF <frankergamer@outlook.com>
2023-06-09 16:01:05 +0000

    Prueba de integración de translatetodo y comentado la actualización de sam-cli que hace que se cuelgue todo el entorno.

--- pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
+++ pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
@@ -5,7 +5,8 @@ python3.7 -m venv todo-list-aws
 source todo-list-aws/bin/activate
 python -m pip install --upgrade pip
 python -m pip install awscli
-python -m pip install aws-sam-cli
+#python -m pip install aws-sam-cli
 # For integration testing
-python -m pip install pytest
+#python -m pip install pytest
+python -m pip install pytest==7.2.0
 pwd
