ea92bc705ea8f8993dc802e080d633fb5ccb142d
frankerF <frankergamer@outlook.com>
2023-06-10 06:54:25 +0000

    Se añade a setup de producción las dependencias que hay en staging

--- pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
+++ pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
@@ -7,6 +7,18 @@ python -m pip install --upgrade pip
 python -m pip install awscli
 #python -m pip install aws-sam-cli
 # For integration testing
-python -m pip install pytest
+#python -m pip install pytest
+#For static testing
+python -m pip install radon==5.1.0
+python -m pip install flake8==5.0.4
+python -m pip install flake8-polyfill==1.0.2
+python -m pip install bandit==1.7.4
+#For integration testing
+python -m pip install pytest==7.2.0
+#For unit testing
+python -m pip install boto3==1.26.27
+python -m pip install moto==4.0.11
+python -m pip install mock==4.0.3
+python -m pip install coverage==6.5.0
 
 pwd
